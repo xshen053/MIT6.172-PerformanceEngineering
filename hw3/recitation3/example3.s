65014c708bfe0b2c66322f330d8ff24724bd1d59
XiaxiShen <shenxiaxi26@gmail.com>
2023-06-21 16:19:26 -0700

    tested example3

--- hw3/recitation3/example3.s
+++ hw3/recitation3/example3.s
@@ -1,53 +1,33 @@
 	.text
-	.file	"example2.c"
+	.file	"example3.c"
 	.globl	test                            # -- Begin function test
 	.p2align	4, 0x90
 	.type	test,@function
 test:                                   # @test
 .Lfunc_begin0:
-	.file	0 "/home/xiaxishen/Desktop/MIT6.172/MIT6.172-PerformanceEngineering/hw3/recitation3" "example2.c" md5 0x31c23e31182425bef06acd74577064d7
-	.loc	0 9 0                           # example2.c:9:0
+	.file	0 "/home/xiaxishen/Desktop/MIT6.172/MIT6.172-PerformanceEngineering/hw3/recitation3" "example3.c" md5 0x1d32677eb9ece2a9cef5048c19df2605
+	.loc	0 9 0                           # example3.c:9:0
 	.cfi_startproc
 # %bb.0:
 	#DEBUG_VALUE: test:a <- $rdi
 	#DEBUG_VALUE: test:b <- $rsi
-	xorl	%eax, %eax
+	pushq	%rax
+	.cfi_def_cfa_offset 16
 .Ltmp0:
 	#DEBUG_VALUE: test:i <- 0
-	.p2align	4, 0x90
-.LBB0_1:                                # =>This Inner Loop Header: Depth=1
-	#DEBUG_VALUE: test:a <- $rdi
-	#DEBUG_VALUE: test:b <- $rsi
-	#DEBUG_VALUE: test:i <- 0
-	.loc	0 18 13 prologue_end            # example2.c:18:13
-	movdqa	(%rsi,%rax), %xmm0
-	movdqa	16(%rsi,%rax), %xmm1
-	.loc	0 18 12 is_stmt 0               # example2.c:18:12
-	pmaxub	(%rdi,%rax), %xmm0
-	pmaxub	16(%rdi,%rax), %xmm1
-	.loc	0 18 10                         # example2.c:18:10
-	movdqa	%xmm0, (%rdi,%rax)
-	movdqa	%xmm1, 16(%rdi,%rax)
-	.loc	0 18 13                         # example2.c:18:13
-	movdqa	32(%rsi,%rax), %xmm0
-	movdqa	48(%rsi,%rax), %xmm1
-	.loc	0 18 12                         # example2.c:18:12
-	pmaxub	32(%rdi,%rax), %xmm0
-	pmaxub	48(%rdi,%rax), %xmm1
-	.loc	0 18 10                         # example2.c:18:10
-	movdqa	%xmm0, 32(%rdi,%rax)
-	movdqa	%xmm1, 48(%rdi,%rax)
+	.loc	0 12 3 prologue_end             # example3.c:12:3
+	addq	$1, %rsi
 .Ltmp1:
-	.loc	0 15 26 is_stmt 1               # example2.c:15:26
-	addq	$64, %rax
-	cmpq	$65536, %rax                    # imm = 0x10000
-	jne	.LBB0_1
+	#DEBUG_VALUE: test:b <- [DW_OP_LLVM_entry_value 1] $rsi
+	.loc	0 13 10                         # example3.c:13:10
+	movl	$65536, %edx                    # imm = 0x10000
+	callq	memcpy@PLT
 .Ltmp2:
-# %bb.2:
-	#DEBUG_VALUE: test:a <- $rdi
-	#DEBUG_VALUE: test:b <- $rsi
-	#DEBUG_VALUE: test:i <- 0
-	.loc	0 20 1                          # example2.c:20:1
+	#DEBUG_VALUE: test:a <- [DW_OP_LLVM_entry_value 1] $rdi
+	#DEBUG_VALUE: test:i <- [DW_OP_plus_uconst 1, DW_OP_stack_value] undef
+	.loc	0 15 1                          # example3.c:15:1
+	popq	%rax
+	.cfi_def_cfa_offset 8
 	retq
 .Ltmp3:
 .Lfunc_end0:
@@ -56,6 +36,56 @@ test:                                   # @test
                                         # -- End function
 	.file	1 "/usr/include/x86_64-linux-gnu/bits" "types.h" md5 0xd108b5f93a74c50510d7d9bc0ab36df9
 	.file	2 "/usr/include/x86_64-linux-gnu/bits" "stdint-uintn.h" md5 0x2bf2ae53c58c01b1a1b9383b5195125c
+	.section	.debug_loclists,"",@progbits
+	.long	.Ldebug_list_header_end0-.Ldebug_list_header_start0 # Length
+.Ldebug_list_header_start0:
+	.short	5                               # Version
+	.byte	8                               # Address size
+	.byte	0                               # Segment selector size
+	.long	3                               # Offset entry count
+.Lloclists_table_base0:
+	.long	.Ldebug_loc0-.Lloclists_table_base0
+	.long	.Ldebug_loc1-.Lloclists_table_base0
+	.long	.Ldebug_loc2-.Lloclists_table_base0
+.Ldebug_loc0:
+	.byte	4                               # DW_LLE_offset_pair
+	.uleb128 .Lfunc_begin0-.Lfunc_begin0    #   starting offset
+	.uleb128 .Ltmp2-.Lfunc_begin0           #   ending offset
+	.byte	1                               # Loc expr size
+	.byte	85                              # DW_OP_reg5
+	.byte	4                               # DW_LLE_offset_pair
+	.uleb128 .Ltmp2-.Lfunc_begin0           #   starting offset
+	.uleb128 .Lfunc_end0-.Lfunc_begin0      #   ending offset
+	.byte	4                               # Loc expr size
+	.byte	163                             # DW_OP_entry_value
+	.byte	1                               # 1
+	.byte	85                              # DW_OP_reg5
+	.byte	159                             # DW_OP_stack_value
+	.byte	0                               # DW_LLE_end_of_list
+.Ldebug_loc1:
+	.byte	4                               # DW_LLE_offset_pair
+	.uleb128 .Lfunc_begin0-.Lfunc_begin0    #   starting offset
+	.uleb128 .Ltmp1-.Lfunc_begin0           #   ending offset
+	.byte	1                               # Loc expr size
+	.byte	84                              # DW_OP_reg4
+	.byte	4                               # DW_LLE_offset_pair
+	.uleb128 .Ltmp1-.Lfunc_begin0           #   starting offset
+	.uleb128 .Lfunc_end0-.Lfunc_begin0      #   ending offset
+	.byte	4                               # Loc expr size
+	.byte	163                             # DW_OP_entry_value
+	.byte	1                               # 1
+	.byte	84                              # DW_OP_reg4
+	.byte	159                             # DW_OP_stack_value
+	.byte	0                               # DW_LLE_end_of_list
+.Ldebug_loc2:
+	.byte	4                               # DW_LLE_offset_pair
+	.uleb128 .Ltmp0-.Lfunc_begin0           #   starting offset
+	.uleb128 .Ltmp2-.Lfunc_begin0           #   ending offset
+	.byte	2                               # Loc expr size
+	.byte	48                              # DW_OP_lit0
+	.byte	159                             # DW_OP_stack_value
+	.byte	0                               # DW_LLE_end_of_list
+.Ldebug_list_header_end0:
 	.section	.debug_abbrev,"",@progbits
 	.byte	1                               # Abbreviation Code
 	.byte	17                              # DW_TAG_compile_unit
@@ -77,6 +107,8 @@ test:                                   # @test
 	.byte	18                              # DW_AT_high_pc
 	.byte	6                               # DW_FORM_data4
 	.byte	115                             # DW_AT_addr_base
+	.byte	23                              # DW_FORM_sec_offset
+	.ascii	"\214\001"                      # DW_AT_loclists_base
 	.byte	23                              # DW_FORM_sec_offset
 	.byte	0                               # EOM(1)
 	.byte	0                               # EOM(2)
@@ -107,7 +139,7 @@ test:                                   # @test
 	.byte	5                               # DW_TAG_formal_parameter
 	.byte	0                               # DW_CHILDREN_no
 	.byte	2                               # DW_AT_location
-	.byte	24                              # DW_FORM_exprloc
+	.byte	34                              # DW_FORM_loclistx
 	.byte	3                               # DW_AT_name
 	.byte	37                              # DW_FORM_strx1
 	.byte	58                              # DW_AT_decl_file
@@ -121,8 +153,8 @@ test:                                   # @test
 	.byte	4                               # Abbreviation Code
 	.byte	52                              # DW_TAG_variable
 	.byte	0                               # DW_CHILDREN_no
-	.byte	28                              # DW_AT_const_value
-	.byte	15                              # DW_FORM_udata
+	.byte	2                               # DW_AT_location
+	.byte	34                              # DW_FORM_loclistx
 	.byte	3                               # DW_AT_name
 	.byte	37                              # DW_FORM_strx1
 	.byte	58                              # DW_AT_decl_file
@@ -180,7 +212,7 @@ test:                                   # @test
 	.byte	1                               # DWARF Unit Type
 	.byte	8                               # Address Size (in bytes)
 	.long	.debug_abbrev                   # Offset Into Abbrev. Section
-	.byte	1                               # Abbrev [1] 0xc:0x73 DW_TAG_compile_unit
+	.byte	1                               # Abbrev [1] 0xc:0x75 DW_TAG_compile_unit
 	.byte	0                               # DW_AT_producer
 	.short	12                              # DW_AT_language
 	.byte	1                               # DW_AT_name
@@ -190,7 +222,8 @@ test:                                   # @test
 	.byte	0                               # DW_AT_low_pc
 	.long	.Lfunc_end0-.Lfunc_begin0       # DW_AT_high_pc
 	.long	.Laddr_table_base0              # DW_AT_addr_base
-	.byte	2                               # Abbrev [2] 0x23:0x29 DW_TAG_subprogram
+	.long	.Lloclists_table_base0          # DW_AT_loclists_base
+	.byte	2                               # Abbrev [2] 0x27:0x27 DW_TAG_subprogram
 	.byte	0                               # DW_AT_low_pc
 	.long	.Lfunc_end0-.Lfunc_begin0       # DW_AT_high_pc
 	.byte	1                               # DW_AT_frame_base
@@ -201,56 +234,54 @@ test:                                   # @test
 	.byte	9                               # DW_AT_decl_line
                                         # DW_AT_prototyped
                                         # DW_AT_external
-	.byte	3                               # Abbrev [3] 0x2e:0xa DW_TAG_formal_parameter
-	.byte	1                               # DW_AT_location
-	.byte	85
+	.byte	3                               # Abbrev [3] 0x32:0x9 DW_TAG_formal_parameter
+	.byte	0                               # DW_AT_location
 	.byte	4                               # DW_AT_name
 	.byte	0                               # DW_AT_decl_file
 	.byte	9                               # DW_AT_decl_line
-	.long	76                              # DW_AT_type
-	.byte	3                               # Abbrev [3] 0x38:0xa DW_TAG_formal_parameter
+	.long	78                              # DW_AT_type
+	.byte	3                               # Abbrev [3] 0x3b:0x9 DW_TAG_formal_parameter
 	.byte	1                               # DW_AT_location
-	.byte	84
 	.byte	8                               # DW_AT_name
 	.byte	0                               # DW_AT_decl_file
 	.byte	9                               # DW_AT_decl_line
-	.long	76                              # DW_AT_type
-	.byte	4                               # Abbrev [4] 0x42:0x9 DW_TAG_variable
-	.byte	0                               # DW_AT_const_value
+	.long	78                              # DW_AT_type
+	.byte	4                               # Abbrev [4] 0x44:0x9 DW_TAG_variable
+	.byte	2                               # DW_AT_location
 	.byte	9                               # DW_AT_name
 	.byte	0                               # DW_AT_decl_file
 	.byte	10                              # DW_AT_decl_line
-	.long	106                             # DW_AT_type
+	.long	108                             # DW_AT_type
 	.byte	0                               # End Of Children Mark
-	.byte	5                               # Abbrev [5] 0x4c:0x5 DW_TAG_restrict_type
-	.long	81                              # DW_AT_type
-	.byte	6                               # Abbrev [6] 0x51:0x5 DW_TAG_pointer_type
-	.long	86                              # DW_AT_type
-	.byte	7                               # Abbrev [7] 0x56:0x8 DW_TAG_typedef
-	.long	94                              # DW_AT_type
+	.byte	5                               # Abbrev [5] 0x4e:0x5 DW_TAG_restrict_type
+	.long	83                              # DW_AT_type
+	.byte	6                               # Abbrev [6] 0x53:0x5 DW_TAG_pointer_type
+	.long	88                              # DW_AT_type
+	.byte	7                               # Abbrev [7] 0x58:0x8 DW_TAG_typedef
+	.long	96                              # DW_AT_type
 	.byte	7                               # DW_AT_name
 	.byte	2                               # DW_AT_decl_file
 	.byte	24                              # DW_AT_decl_line
-	.byte	7                               # Abbrev [7] 0x5e:0x8 DW_TAG_typedef
-	.long	102                             # DW_AT_type
+	.byte	7                               # Abbrev [7] 0x60:0x8 DW_TAG_typedef
+	.long	104                             # DW_AT_type
 	.byte	6                               # DW_AT_name
 	.byte	1                               # DW_AT_decl_file
 	.byte	38                              # DW_AT_decl_line
-	.byte	8                               # Abbrev [8] 0x66:0x4 DW_TAG_base_type
+	.byte	8                               # Abbrev [8] 0x68:0x4 DW_TAG_base_type
 	.byte	5                               # DW_AT_name
 	.byte	8                               # DW_AT_encoding
 	.byte	1                               # DW_AT_byte_size
-	.byte	7                               # Abbrev [7] 0x6a:0x8 DW_TAG_typedef
-	.long	114                             # DW_AT_type
+	.byte	7                               # Abbrev [7] 0x6c:0x8 DW_TAG_typedef
+	.long	116                             # DW_AT_type
 	.byte	12                              # DW_AT_name
 	.byte	2                               # DW_AT_decl_file
 	.byte	27                              # DW_AT_decl_line
-	.byte	7                               # Abbrev [7] 0x72:0x8 DW_TAG_typedef
-	.long	122                             # DW_AT_type
+	.byte	7                               # Abbrev [7] 0x74:0x8 DW_TAG_typedef
+	.long	124                             # DW_AT_type
 	.byte	11                              # DW_AT_name
 	.byte	1                               # DW_AT_decl_file
 	.byte	45                              # DW_AT_decl_line
-	.byte	8                               # Abbrev [8] 0x7a:0x4 DW_TAG_base_type
+	.byte	8                               # Abbrev [8] 0x7c:0x4 DW_TAG_base_type
 	.byte	10                              # DW_AT_name
 	.byte	7                               # DW_AT_encoding
 	.byte	8                               # DW_AT_byte_size
@@ -265,7 +296,7 @@ test:                                   # @test
 .Linfo_string0:
 	.asciz	"Ubuntu clang version 14.0.0-1ubuntu1" # string offset=0
 .Linfo_string1:
-	.asciz	"example2.c"                    # string offset=37
+	.asciz	"example3.c"                    # string offset=37
 .Linfo_string2:
 	.asciz	"/home/xiaxishen/Desktop/MIT6.172/MIT6.172-PerformanceEngineering/hw3/recitation3" # string offset=48
 .Linfo_string3:
